a037eb5f5be98375a9a4bf712055b995fd5b927b
Mateus Schiavi <mtsschiavi@gmail.com>
2023-10-18 10:18:03 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Warning from './components/Warning'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <Warning/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Warning.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Warning.css'
+function Warning() {
+  return (
+    <div className='warning'>
+      <p>
+      <strong>404</strong> | This page could not be found
+      </p>
+    </div>
+  )
+}
+
+export default Warning
